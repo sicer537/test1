10f6985e327dca5ee3d366d8a83277cb385ef0f7
sicer537 <artemdor2009@gmail.com>
2025-03-22 13:05:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+'use strict'
+
+function slider(){
+    const sliderDiv = document.getElementById("slider")
+    const sliderImages = sliderDiv.querySelectorAll('img')
+    sliderImages.forEach( img => img.onclick = moveSlide )
+
+    const slideAnimationTimeout = 400 // длительность CSS-перехода
+
+    function moveSlide(event) {
+        event.target.classList.add('move-left')
+        setTimeout( movSlideBack, slideAnimationTimeout, event.target )
+    }
+    function movSlideBack(slide) {
+        slide.remove() // удаляем слайд
+        sliderDiv.prepend(slide) // перемещаем в начало (так как виден при наложении блоков последний)
+        slide.classList.remove('move-left') // убираем класс, что бы слайд вернулся на место
+    }
+    let slideindex = sliderImages.length
+    const autosliding = 5000
+    setInterval(nextslide, autosliding)
+
+    function updateslideindex(){
+        slideindex--
+        if(slideindex < 0) slideindex = sliderImages.length - 1
+    }
+    function nextslide(){
+        updateslideindex()
+        sliderImages[slideindex].classList.add('move-left')
+        setTimeout(movSlideBack, slideAnimationTimeout, sliderImages[slideindex])
+    }
+}
+slider()
+function timer() {
+    function updateSaleDate() {
+        let rest = endSaleTime - Date.now()
+        let days = Math.floor(rest / dayMilliseconds)
+        rest -= days * dayMilliseconds
+        let hours = Math.floor(rest / hourMilliseconds)
+        rest -= hours * hourMilliseconds
+        let minutes = Math.floor(rest / minuteMilliseconds)
+        rest -= minutes * minuteMilliseconds
+        let seconds = Math.ceil(rest / 1000)
+
+        saleDaysSpan.innerText = days
+        saleHoursSpan.innerText = hours
+        saleMinutesSpan.innerText = formatTo00(minutes)
+        saleSecondsSpan.innerText = formatTo00(seconds)
+    }
+
+    function formatTo00(number) {
+        if (number < 10) return '0' + number
+        return number
+    }
+}
+timer()
+function popUp(){
+const popUp = document.getElementById('popUp')
+popUp.onclick = () => popUp.style.display = 'none'}
